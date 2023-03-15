7ec2c675d84c73fe534fba24108434fdbe5e1ac5
Maria Paula <mariapaula.torres64@gmail.com>
2023-03-15 00:23:53 -0300

    feat: create functions to return standard HTTP responses

---
+++ src/helpers/messageResponseHelpers.js
@@ -0,0 +1,51 @@
+function messageResponse(status, message, res, data) {
+  return res.status(status).json({ status, message, ...(data && { data }) });
+}
+
+function onSucess(message, res, data) {
+  return messageResponse(200, message, res, data);
+}
+
+function created(message, res, data) {
+  return messageResponse(201, message, res, data);
+}
+
+function onSucessNoContent(message, res) {
+  return messageResponse(204, message, res);
+}
+
+function badRequest(message, res) {
+  return messageResponse(400, message, res);
+}
+
+function unauthorized(message, res) {
+  return messageResponse(401, message, res);
+}
+
+function forbidden(message, res) {
+  return messageResponse(403, message, res);
+}
+
+function notFound(message, res) {
+  return messageResponse(404, message, res);
+}
+
+function internalServerError(message, res) {
+  return messageResponse(500, message, res);
+}
+
+function serviceUnavailable(message, res) {
+  return messageResponse(503, message, res);
+}
+
+module.exports = {
+  onSucess,
+  created,
+  onSucessNoContent,
+  badRequest,
+  unauthorized,
+  forbidden,
+  notFound,
+  internalServerError,
+  serviceUnavailable,
+};

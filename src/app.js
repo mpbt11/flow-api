89b12c3d08a37243046d41a0a55aa160e6d39650
Maria Paula <mariapaula.torres64@gmail.com>
2023-03-15 00:28:34 -0300

    refactor: renamed file and optimized HTTP server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cashflow = require("./routes/cashFlow")
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/v1/cashflow", cashflow);
+
+module.exports = app;

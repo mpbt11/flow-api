9ce85ff0df28fbc9fc2fcb6fd1425cc87e8254bc
Maria Paula <mariapaula.torres64@gmail.com>
2023-04-01 17:50:27 -0300

    refactor: add createValidator for PUT requests in cashFlow routes

--- src/routes/cashFlow.js
+++ src/routes/cashFlow.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const cashFlowController = require('../controllers/cashFlowController');
-const { createValidatorsBody, createValidatorsParams } = require('../services/cashFlow/validators');
+const { createValidatorsBody, createValidatorsParams, createValidator } = require('../services/cashFlow/validators');
 
 router.post('/', createValidatorsBody, cashFlowController.create);
-router.put('/:id_flow_cash', createValidatorsBody, cashFlowController.update);
+router.put('/:id_flow_cash', createValidator, cashFlowController.update);
 router.get('/', cashFlowController.get);
 router.get('/:id_flow_cash', createValidatorsParams, cashFlowController.getById);
 router.delete('/:id_flow_cash', createValidatorsParams, cashFlowController.delete);

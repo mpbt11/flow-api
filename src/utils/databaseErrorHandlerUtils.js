f253d9a77b295bd1f8ccbc491d71679b9137c684
Maria Paula <mariapaula.torres64@gmail.com>
2023-04-01 17:47:30 -0300

    refactor: reorganize error handling with the switch statement

--- src/utils/databaseErrorHandlerUtils.js
+++ src/utils/databaseErrorHandlerUtils.js
@@ -4,40 +4,44 @@ const {
 } = require("../helpers/messageResponseHelpers");
 
 const handleDatabaseError = (error, res) => {
-  if (error.code === "23505") {
-    return badRequest("Erro de violação de chave!", res);
+  
+  switch (error.code) {
+    case "23505":
+      return badRequest("Erro de violação de chave!", res);
+
+    case "23502":
+      return badRequest("Valor nulo inserido em coluna não permitida!", res);
+
+    case "22P02":
+      return badRequest("Valor inválido para tipo de dado!", res);
+
+    case "42703":
+      return badRequest("Coluna não encontrada!", res);
+
+    case "23503":
+      return badRequest("Violação de chave estrangeira!", res);
+
+    case "23514":
+      return badRequest("Verificação de domínio falhou!", res);
+
+    case "22001":
+      return badRequest(
+        "Comprimento do valor excede o tamanho máximo para a coluna!",
+        res
+      );
+
+    case "22003":
+      return badRequest(
+        "Valor fora do intervalo válido para o tipo de dado!",
+        res
+      );
+
+    case "22007":
+      return badRequest("Valor de data/hora inválido!", res);
+
+    default:
+      return internalServerError("Erro interno do servidor!", res);
   }
-  if (error.code === "23502") {
-    return badRequest("Valor nulo inserido em coluna não permitida!", res);
-  }
-  if (error.code === "22P02") {
-    return badRequest("Valor inválido para tipo de dado!", res);
-  }
-  if (error.code === "42703") {
-    return badRequest("Coluna não encontrada!", res);
-  }
-  if (error.code === "23503") {
-    return badRequest("Violacão de chave estrangeira!", res);
-  }
-  if (error.code === "23514") {
-    return badRequest("Verificação de domínio falhou!", res);
-  }
-  if (error.code === "22001") {
-    return badRequest(
-      "Comprimento do valor excede o tamanho máximo para a coluna!",
-      res
-    );
-  }
-  if (error.code === "22003") {
-    return badRequest(
-      "Valor fora do intervalo válido para o tipo de dado!",
-      res
-    );
-  }
-  if (error.code === "22007") {
-    return badRequest("Valor de data/hora inválido!", res);
-  }
-  return internalServerError("Erro interno do servidor!", res);
 };
 
 module.exports = {

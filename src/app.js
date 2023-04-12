120c2433a43e7b868beb27e4ee43222c87f87285
Maria Paula <mariapaula.torres64@gmail.com>
2023-04-11 21:50:45 -0300

    feat: add uploadAws

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const cashflow = require("./routes/cashFlow")
+const uploadAws = require("./routes/uploadAws");
 
 dotenv.config();
 const app = express();
@@ -12,5 +12,6 @@ app.use(bodyParser.json());
 
 app.use("/api/v1/cashflow", cashflow);
 app.use("/api/v1/category", category);
+app.use("/api/v1/upload-aws", uploadAws);
 
 module.exports = app;
